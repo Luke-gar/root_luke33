8dfcc4989ca815d9556261190d8ede6b9e510cc6
Luke-gar <6291933@edu.vaniercollege.qc.ca>
2025-04-04 08:02:00 -0400

    finailizing my spa and stuff

--- public_html/web2/javascript/spa.js
+++ public_html/web2/javascript/spa.js
@@ -1,9 +1,38 @@
-console.log("ready to go")
-const fadein = document.querySelectorAll(".hidden");
-fadein.forEach((item) => {
-    item.addEventListener("mouseenter", function () {
+console.log("ready to go");
+//Bruce says use intersection observer https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
+const fadeIn = document.querySelectorAll(".subtitle");
+const textContainers = document.querySelectorAll('.container_text');
+console.log(fadeIn);
+fadeIn.forEach((item) => {
+    
+    item.addEventListener("click", function (event) {
+        console.log(event.target.parentNode.nextElementSibling.querySelector('p'));
         console.log("stage1 good");
-        item.classList.add("visable");
+        event.target.parentNode.nextElementSibling.querySelector('p').classList.add("visible");
     })
 }) 
 
+const sectionfadein = document.querySelectorAll('.section2_hidden')
+console.log(sectionfadein);
+sectionfadein.forEach((item) => {
+    item.addEventListener("mouseenter", function (event) {
+        console.log(event.target.querySelector('section2_hidden'));
+        console.log("stage1 set")
+        if(item.classList.contains("section2_hidden")) {
+            item.classList.add("visible")
+        }   
+    })
+})
+
+const drawin = document.querySelectorAll('drawing')
+console.log(drawin);
+drawin.forEach((item) => {
+    item.addEventListener("click", function (event) {
+        console.log(event.target.querySelector('drawing'));
+        console.log("set and ready to go")
+        if(item.classList.contains(".drawing")){
+            item.classList.add("anime")
+        }
+    } ) 
+})
